cc0175d778eec6929b7e69abd93d159af87aa5d6
MartaDiazCoello <diazcoellomarta5@gmail.com>
2024-05-08 20:37:28 +0200

    Update ResourceException.java

--- Proyecto/src/planetWars/ResourceException.java
+++ Proyecto/src/planetWars/ResourceException.java
@@ -2,10 +2,6 @@ package planetWars;
 
 public class ResourceException extends Exception {
 
-    /**
-     * Constructor que acepta un mensaje descriptivo que explica la razón de la excepción.
-     * @param message Mensaje descriptivo sobre la causa de la excepción.
-     */
     public ResourceException(String message) {
         super(message);  // Llama al constructor de la superclase Exception.
     }
