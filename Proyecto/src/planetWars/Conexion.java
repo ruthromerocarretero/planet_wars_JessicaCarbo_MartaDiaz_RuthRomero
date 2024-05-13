be1a492b84cd92cb9af05db9d3149c2a9c7755da
Yessc <117772270+Yessc@users.noreply.github.com>
2024-05-13 19:59:55 +0200

    creo conexion base de datos

--- Proyecto/src/planetWars/Conexion.java
+++ Proyecto/src/planetWars/Conexion.java
@@ -9,7 +9,8 @@ import java.sql.Statement;
 public class Conexion {
 
     public static void main(String[] args) {
-        // Datos de conexión a la base de datos
+        
+    	// Datos para la  conexión a la base de datos
     	
     	String urlDatos = "jdbc:oracle:thin:@localhost:1521/orcl";
         String usuario = "admin";
@@ -24,32 +25,19 @@ public class Conexion {
         	Statement statement = conn.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
             System.out.println("Conexión establecida correctamente.");
-         // Confirmar que la consulta se ha ejecutado correctamente
+         
             System.out.println("Consulta ejecutada correctamente.");
 
-            // Iterar sobre los resultados y mostrarlos
-            while (resultSet.next()) {
-                // Suponiendo que la tabla tiene columnas "columna1", "columna2", etc.
-                String columna1 = resultSet.getString("columna1");
-                String columna2 = resultSet.getString("columna2");
-                // Obtén valores de otras columnas según sea necesario
-
-                // Imprimir los valores de las columnas
-                System.out.println("Columna1: " + columna1 + ", Columna2: " + columna2);
-                // Imprime valores de otras columnas según sea necesario
-            }
-
-            // Cerrar recursos
+       
             resultSet.close();
             statement.close();
             conn.close();
 
    
 
-            // Realizar operaciones adicionales con la base de datos aquí
 
         } catch (SQLException e) {
-            // Capturar excepciones de SQL y mostrar un mensaje de error
+            // este catch es para los errores que puedan surgir
             System.out.println("Error al conectar con la base de datos: " + e.getMessage());
       
         }
