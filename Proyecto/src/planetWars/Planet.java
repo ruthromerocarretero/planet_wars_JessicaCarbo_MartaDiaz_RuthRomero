4b36a3f719193d854735d4bb7d171a96bd08f006
Yessc <117772270+Yessc@users.noreply.github.com>
2024-05-14 00:11:28 +0200

    se ha creado los metodos para calcular las naves que se pueden tener

--- Proyecto/src/planetWars/Planet.java
+++ Proyecto/src/planetWars/Planet.java
@@ -39,8 +39,11 @@ public class Planet {
 	
 	
 	 public void upgradeTechnologyDefense() throws ResourceException {
+		// se calcula el costo de la actualización actual
+		    int currentUpgradeCost = upgradeDefenseTechnologyDeuteriumCost;
 	        if (deuterium >= upgradeDefenseTechnologyDeuteriumCost) { // Verifica si hay suficiente deuterium
-	            deuterium -= upgradeDefenseTechnologyDeuteriumCost; // Si se cumple, le resta el coste de la mejora
+	        	deuterium -= currentUpgradeCost;
+	        	// Si se cumple, le resta el coste de la mejora
 	            technologyDefense++; // Lo incrementa en uno
 	            upgradeDefenseTechnologyDeuteriumCost *= 1.1;  // Incrementa el costo en un 10%
 	        } else { // Si no se cumple la condicion, entonces lanza una excepcion pq no hay suficiente "Deuterium" 
@@ -49,8 +52,9 @@ public class Planet {
 	    }
 	 
 	 public void upgradeTechnologyAttack() throws ResourceException {
-	        if (deuterium >= upgradeAttackTechnologyDeuteriumCost) {
-	            deuterium -= upgradeAttackTechnologyDeuteriumCost;
+		    int currentUpgradeCost = upgradeAttackTechnologyDeuteriumCost;
+	        if (deuterium >=  currentUpgradeCost) {
+	            deuterium -=  currentUpgradeCost);
 	            technologyAtack++;
 	            upgradeAttackTechnologyDeuteriumCost *= 1.1;  
 	        } else {
@@ -70,30 +74,102 @@ public class Planet {
 	                army[0].add(new LigthHunter(i, i));  
 	            }
 	        } else {
-	            throw new ResourceException("No hay suficientes recursos para producir " + n + " cazadores ligeros.");
+	            throw new ResourceException("No hay suficientes recursos para producir " + n + " LightHunter.");
 	        }
 	    }
 	 
-	public void newHeavyHunter(int n) {
-		
-	}
-	public void newBattleShip(int n) {
-		
-	}
+	public void newHeavyHunter(int n) throws ResourceException {
+        int totalMetalCost = Variables.METAL_COST_HEAVYHUNTER * n;
+        int totalDeuteriumCost = Variables.DEUTERIUM_COST_HEAVYHUNTER * n;
+
+        if (metal >= totalMetalCost && deuterium >= totalDeuteriumCost) {
+            metal -= totalMetalCost;
+            deuterium -= totalDeuteriumCost;
+
+            for (int i = 0; i < n; i++) {
+                army[1].add(new HeavyHunter(i, i));  
+            }
+        } else {
+            throw new ResourceException("No hay suficientes recursos para producir " + n + " HeavyHunter.");
+        }
+    }
+	public void newBattleShip(int n) throws ResourceException {
+        int totalMetalCost = Variables.METAL_COST_BATTLESHIP * n;
+        int totalDeuteriumCost = Variables.DEUTERIUM_COST_BATTLESHIP * n;
+
+        if (metal >= totalMetalCost && deuterium >= totalDeuteriumCost) {
+            metal -= totalMetalCost;
+            deuterium -= totalDeuteriumCost;
+
+            for (int i = 0; i < n; i++) {
+                army[2].add(new BattleShip(i, i));  
+            }
+        } else {
+            throw new ResourceException("No hay suficientes recursos para producir " + n + " BattleShip.");
+        }
+    }
 	
-	public void newArmoredShip(int n) {
-		
-	}
+	public void newArmoredShip(int n) throws ResourceException {
+        int totalMetalCost = Variables.METAL_COST_ARMOREDSHIP * n;
+        int totalDeuteriumCost = Variables.DEUTERIUM_COST_ARMOREDSHIP * n;
+
+        if (metal >= totalMetalCost && deuterium >= totalDeuteriumCost) {
+            metal -= totalMetalCost;
+            deuterium -= totalDeuteriumCost;
+
+            for (int i = 0; i < n; i++) {
+                army[3].add(new ArmoredShip(i, i));  
+            }
+        } else {
+            throw new ResourceException("No hay suficientes recursos para producir " + n + " ArmoredShip.");
+        }
+    }
 	
-	public  void newMissileLauncher(int n) {
-		
-	}
-	public void newIonCannon(int n) {
-		
-	}
-	public void newPlasmaCannon(int n) {
-		
-	}
+	public  void newMissileLauncher(int n) throws ResourceException {
+        int totalMetalCost = Variables.METAL_COST_MISSILELAUNCHER  * n;
+        int totalDeuteriumCost = Variables.DEUTERIUM_COST_MISSILELAUNCHER * n;
+
+        if (metal >= totalMetalCost && deuterium >= totalDeuteriumCost) {
+            metal -= totalMetalCost;
+            deuterium -= totalDeuteriumCost;
+
+            for (int i = 0; i < n; i++) {
+                army[4].add(new MissileLauncher(i, i));  
+            }
+        } else {
+            throw new ResourceException("No hay suficientes recursos para producir " + n + " MissileLauncher.");
+        }
+    }
+	public void newIonCannon(int n) throws ResourceException {
+        int totalMetalCost = Variables.METAL_COST_IONCANNON  * n;
+        int totalDeuteriumCost = Variables.DEUTERIUM_COST_IONCANNON * n;
+
+        if (metal >= totalMetalCost && deuterium >= totalDeuteriumCost) {
+            metal -= totalMetalCost;
+            deuterium -= totalDeuteriumCost;
+
+            for (int i = 0; i < n; i++) {
+                army[5].add(new IonCannon(i, i));  
+            }
+        } else {
+            throw new ResourceException("No hay suficientes recursos para producir " + n + " IonCannon.");
+        }
+    }
+	public void newPlasmaCannon(int n)  throws ResourceException {
+        int totalMetalCost = Variables.METAL_COST_PLASMACANNON * n;
+        int totalDeuteriumCost = Variables.DEUTERIUM_COST_PLASMACANNON * n;
+
+        if (metal >= totalMetalCost && deuterium >= totalDeuteriumCost) {
+            metal -= totalMetalCost;
+            deuterium -= totalDeuteriumCost;
+
+            for (int i = 0; i < n; i++) {
+                army[6].add(new PlasmaCannon(i, i));  
+            }
+        } else {
+            throw new ResourceException("No hay suficientes recursos para producir " + n + " PlasmaCannon.");
+        }
+    }
 	public int getTechnologyDefense() {
 		return technologyDefense;
 	}
@@ -151,9 +227,24 @@ public class Planet {
 	}
 	
 	public void printStats() {
-		System.out.println("Planet Stats");
-		System.out.println("TECHNOLOGY");
-		
+	    System.out.println("Planet Stats");
+	    System.out.println("TECHNOLOGY");
+	    System.out.println(String.format("%-20s %20d", "Atack Technology:", technologyAtack)); // Corrección aquí
+	    System.out.println(String.format("%-20s %20d", "Defense Technology:", technologyDefense)); // Corrección aquí
+	    System.out.println("DEFENSES");
+	    System.out.println(String.format("%-20s %20d", "Missile Launcher:", army[4].size()));
+	    System.out.println(String.format("%-20s %20d", "Ion Cannon:", army[5].size()));
+	    System.out.println(String.format("%-20s %20d", "Plasma Cannon:", army[6].size()));
+	    System.out.println("FLEET");
+	    System.out.println(String.format("%-20s %20d", "Ligh Hunter:", army[0].size()));
+	    System.out.println(String.format("%-20s %20d", "Heavy Hunter:", army[1].size()));
+	    System.out.println(String.format("%-20s %20d", "Battle Ship:", army[2].size()));
+	    System.out.println(String.format("%-20s %20d", "Armored Ship:", army[3].size()));
+	    System.out.println("RESOURCES");
+	    System.out.println(String.format("%-20s %20d", "METAL:", metal));
+	    System.out.println(String.format("%-20s %20d", "DEUTERIUM:", deuterium));
 	}
+
+	
 
 }
