0416e7f42551b92e7770a9ae86a0015431e9dbf1
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2024-05-14 22:28:40 +0200

    cambio de variables
    ajuste de variables para que funcione correctamente el codigo

--- Java/src/planetWars/LigthHunter.java
+++ Java/src/planetWars/LigthHunter.java
@@ -7,16 +7,16 @@ public class LigthHunter extends Ship{
 	public LigthHunter(int armor, int baseDamage) {
 		super(armor, baseDamage);
 // TODO de donde saco nivelTecnologia? technologyDefense del planeta? hay que hacerlo en las dos
-		setArmor(ARMOR_LIGTHHUNTER + (nivelTecnologiaDefensa*PLUS_ARMOR_LIGTHHUNTER_BY_TECHNOLOGY)*1000/100);
-		setBaseDamage(BASE_DAMAGE_LIGTHHUNTER + (nivelTecnologiaAtaque*PLUS_ATTACK_LIGTHHUNTER_BY_TECHNOLOGY)*1000/100);
+		setArmor(ARMOR_LIGTHHUNTER + (technologyDefense*PLUS_ARMOR_LIGTHHUNTER_BY_TECHNOLOGY)*1000/100);
+		setBaseDamage(BASE_DAMAGE_LIGTHHUNTER + (technologyAttack*PLUS_ATTACK_LIGTHHUNTER_BY_TECHNOLOGY)*1000/100);
 		setInitialArmor(armor);
 	}
 
 	public LigthHunter() {
-		super(armor, baseDamage);
+		super();
 		setArmor(ARMOR_LIGTHHUNTER);
 		setBaseDamage(BASE_DAMAGE_LIGTHHUNTER);
-		getInitialArmor(armor);
+		setInitialArmor(getArmor());
 	}
 
 //	TODO Cada una de estas clases tendrá que implementar los métodos definidos en la interfaz MilitaryUnit
---
+++ Java/src/planetWars/Ship.java
@@ -0,0 +1,85 @@
+package planetWars;
+
+public abstract class Ship implements MilitaryUnit, Variables {
+
+    private int armor;
+    private int initialArmor;
+    private int baseDamage;
+
+    public Ship(int armor, int initialArmor, int baseDamage) {
+        super();
+        this.armor = armor;
+        this.initialArmor = initialArmor;
+        this.baseDamage = baseDamage;
+    }
+
+
+
+    // Implementación de los métodos de MilitaryUnit
+    @Override
+    public int attack() {
+
+        return getBaseDamage();  // Simplemente retorna el daño base.
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        this.armor = Math.max(0, this.armor - damage);  // Resta el daño y asegura que el armor no sea negativo.
+    }
+
+    @Override
+    public int getActualArmor() {
+        return getArmor();
+    }
+
+    @Override
+    public void resetArmor() {
+        setArmor(getInitialArmor());
+    }
+
+    // Los siguientes métodos dependen de detalles específicos no proporcionados
+    @Override
+    public int getMetalCost() {
+        return 0;  // Ejemplo, necesita detalles específicos.
+    }
+
+    @Override
+    public int getDeuteriumCost() {
+        return 0;  // Ejemplo, necesita detalles específicos.
+    }
+
+    @Override
+    public int getChanceGeneratinWaste() {
+        return 0;  // Ejemplo, necesita detalles específicos.
+    }
+
+    @Override
+    public int getChanceAttackAgain() {
+        return 0;  // Ejemplo, necesita detalles específicos.
+    }
+
+    // Getters y Setters
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public int getInitialArmor() {
+        return initialArmor;
+    }
+
+    public void setInitialArmor(int initialArmor) {
+        this.initialArmor = initialArmor;
+    }
+
+    public int getBaseDamage() {
+        return baseDamage;
+    }
+
+    public void setBaseDamage(int baseDamage) {
+        this.baseDamage = baseDamage;
+    }
+}
