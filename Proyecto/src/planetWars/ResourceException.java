50a37df7557f72a9281294172180d485bc63a779
MartaDiazCoello <diazcoellomarta5@gmail.com>
2024-05-15 16:16:06 +0200

    u

--- Proyecto/src/planetWars/ResourceException.java
+++ Proyecto/src/planetWars/ResourceException.java
@@ -1,8 +1,4 @@
 package planetWars;
 
-public class ResourceException extends Exception {
-
-    public ResourceException(String message) {
-        super(message);  // Llama al constructor de la superclase Exception.
-    }
+public class ResourceException {
 }
