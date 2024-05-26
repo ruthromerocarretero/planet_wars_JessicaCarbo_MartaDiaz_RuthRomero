001d83cb519f56496f6342b197563320059bca76
Yessc <117772270+Yessc@users.noreply.github.com>
2024-05-27 00:06:49 +0200

    Delete Proyecto directory

--- Proyecto/src/planetWars/ResourceException.java
+++
@@ -1,8 +0,0 @@
-package planetWars;
-
-public class ResourceException extends Exception {
-
-    public ResourceException(String message) {
-        super(message);  // Llama al constructor de la superclase Exception.
-    }
-}
